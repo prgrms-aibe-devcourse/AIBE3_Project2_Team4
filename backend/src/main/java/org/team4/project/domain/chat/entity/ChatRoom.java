8a086158bf43ae14d124efb820042ad16333b4da
Chan <rlackswhd200@gmail.com>
2025-10-01 19:09:28 +0900

    실시간 채팅(전송) 구현 (#8)
    * ✨ feat : 실시간 채팅 기본 구현
    
    * 채팅 실시간 전송 구현(프론트+백)
    
    * clean
    
    * clean2
    
    * clean3
    
    * clean4

---
+++ backend/src/main/java/org/team4/project/domain/chat/entity/ChatRoom.java
@@ -0,0 +1,16 @@
+package org.team4.project.domain.chat.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.team4.project.global.jpa.entity.BaseEntity;
+
+@Data
+@Entity
+public class ChatRoom extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
