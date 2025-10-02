677fe9a11c7c9503e43a3384fb11506c3b818321
GeunYeong <jgy091411@gmail.com>
2025-10-02 14:05:48 +0900

    Feat/payment redis (#16)
    * :heavy_plus_sign: chore : 타임리프 및 결제 페이지 설정 (테스트용, 추후 제거 예정)
    
    * :wrench: chore : API secret key 설정
    
    * :sparkles: feat : 결제 API http client 설정
    
    * :sparkles: feat : 결제 API 연동 (임시)
    
    * :bug: fix : 컨트롤러 @Valid 누락 수정
    
    * :wrench: chore : 레디스 사용 기본 설정
    
    * :sparkles: feat : 결제 요청 전 결제 데이터 레디스 임시 저장 기능
    
    * :sparkles: feat : 결제 승인 요청 전 레디스에 임시 저장된 값과 동일한지 검증
    
    * :sparkles: feat : 커스텀 결제 처리 중 예외 추가 및 설정
    
    * :bug: fix : 레디스 빈 등록 이름 수정

--- backend/src/main/java/org/team4/project/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/org/team4/project/global/exception/GlobalExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.team4.project.domain.member.exception.RegisterException;
+import org.team4.project.domain.payment.exception.PaymentException;
 
 @RestControllerAdvice
 @Slf4j
@@ -32,6 +33,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(RegisterException.class)
     public ResponseEntity<ErrorResponse> handleRegisterException(RegisterException e, HttpServletRequest request) {
         log.warn("Register error at [{} {}]: {}",
+                request.getMethod(), request.getRequestURI(), e.getMessage(), e);
+
+        return new ResponseEntity<>(ErrorResponse.of(request, e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(PaymentException.class)
+    public ResponseEntity<ErrorResponse> handlePaymentException(PaymentException e, HttpServletRequest request) {
+        log.warn("Payment error at [{} {}]: {}",
                 request.getMethod(), request.getRequestURI(), e.getMessage(), e);
 
         return new ResponseEntity<>(ErrorResponse.of(request, e.getMessage()), HttpStatus.BAD_REQUEST);
