e269d1431f15395b43028733d184acb8495bb7cc
AEU <aeuplayground@gmail.com>
2025-10-10 15:49:35 +0900

    :memo: docs : 스웨서 추가 설정

--- backend/src/main/java/org/team4/project/global/springdoc/SpringDocConfig.java
+++ backend/src/main/java/org/team4/project/global/springdoc/SpringDocConfig.java
@@ -2,11 +2,31 @@ package org.team4.project.global.springdoc;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "Team4 Project-2 API Docs", version = "1.0", description = "4팀 2차 프로젝트 API 문서입니다."))
 public class SpringDocConfig {
 
+    @Bean
+    public GroupedOpenApi groupApiV1() {
+        return GroupedOpenApi.builder()
+                .group("apiV1")
+                .pathsToMatch("/api/v1/**")
+                .build();
+    }
+
+    @Bean GroupedOpenApi GroupNone() {
+        return GroupedOpenApi.builder()
+                .group("none-group")
+                .pathsToExclude("/api/**")
+                .pathsToMatch("/**")
+                .build();
+    }
+
     //TODO : 인증 처리 등 더 자세한 설정
+
+
 }
