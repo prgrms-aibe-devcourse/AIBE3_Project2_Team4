9329738e9562137c0237bd7600c1f1921a862c16
dvlplee <dvlplee16@gmail.com>
2025-10-13 01:44:35 +0900

    ✨ S3 파일 업로드/수정/삭제 구현

---
+++ backend/src/main/java/org/team4/project/domain/file/exception/FileUploadException.java
@@ -0,0 +1,10 @@
+package org.team4.project.domain.file.exception;
+
+public class FileUploadException extends RuntimeException {
+    public FileUploadException(String message) {
+        super(message);
+    }
+    public FileUploadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ backend/src/main/java/org/team4/project/domain/file/repository/FileRepository.java
@@ -0,0 +1,12 @@
+package org.team4.project.domain.file.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.team4.project.domain.file.entity.File;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FileRepository extends JpaRepository<File, Long> {
+    Optional<File> findByIdAndFileCategoryAndReferenceId(Long id, String fileCategory, Long referenceId);
+    List<File> findByFileCategoryAndReferenceId(String fileCategory, Long referenceId);
+}
