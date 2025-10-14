0e9b334df83ea249b606a4fb9ec9636877db1a1c
ehgk4245 <ehgkrk14@naver.com>
2025-10-14 18:24:12 +0900

    :memo: docs : Swagger로 API 문서화

--- backend/src/main/java/org/team4/project/domain/member/dto/request/MemberRoleRequestDTO.java
+++ backend/src/main/java/org/team4/project/domain/member/dto/request/MemberRoleRequestDTO.java
@@ -1,5 +1,6 @@
 package org.team4.project.domain.member.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
@@ -7,7 +8,8 @@ import lombok.Data;
 @Data
 public class MemberRoleRequestDTO {
 
+    @Schema(description = "설정할 역할 (CLIENT 또는 FREELANCER)", example = "CLIENT")
     @NotBlank(message = "역할(Role)은 필수 입력입니다.")
     @Pattern(regexp = "CLIENT|FREELANCER", message = "역할은 CLIENT, FREELANCER 중 하나여야 합니다.")
-    String role;
+    private String role;
 }
