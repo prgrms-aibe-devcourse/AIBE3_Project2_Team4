1508e550afc8a42a57cb19214d20c71ee18bace1
GeunYeong <jgy091411@gmail.com>
2025-10-15 12:35:29 +0900

    :sparkles: feat : API 로깅 기능

---
+++ backend/src/main/java/org/team4/project/global/app/RequestCachingFilter.java
@@ -0,0 +1,22 @@
+package org.team4.project.global.app;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Order(value = Ordered.HIGHEST_PRECEDENCE)
+public class RequestCachingFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        filterChain.doFilter(new CachedHttpServletRequest(request), response);
+    }
+}
