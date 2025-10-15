8beaf318b0e3f03a32919316a630d13cc9b135dd
GeunYeong <jgy091411@gmail.com>
2025-10-15 17:07:54 +0900

    :bug: fix : /api 요청만 로깅하도록 수정

--- backend/src/main/java/org/team4/project/global/app/ApiLoggingFilter.java
+++ backend/src/main/java/org/team4/project/global/app/ApiLoggingFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.util.PatternMatchUtils;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 
@@ -15,6 +16,11 @@ import java.nio.charset.StandardCharsets;
 @Slf4j
 @Component
 public class ApiLoggingFilter extends CommonsRequestLoggingFilter {
+
+    @Override
+    protected boolean shouldLog(HttpServletRequest request) {
+        return super.shouldLog(request) && PatternMatchUtils.simpleMatch("/api/**", request.getRequestURI());
+    }
 
     @Override
     protected int getMaxPayloadLength() {
